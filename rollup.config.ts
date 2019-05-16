aad9b926aefb28a8eb089711374ed93c3bf9c139
Rémi Berson <remi.berson@pm.me>
2019-05-16 13:01:00 +0200

    Produce optimized and minified bundles.
    * Add closure compiler plugin to rollup
    * Remove dependency to tslib
    * Produce minified bundles
    * Produce optimized bundles
    * Change default entrypoints in package.json

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,28 +1,62 @@
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
 
-export default [
-  {
-    input: './build/tsmaz.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: pkg.name,
+const plugins = [resolve(), commonjs()];
+
+function tasks(bundleName) {
+  return [
+    // UMD
+    {
+      input: `./build/${bundleName}.js`,
+      output: {
+        file: `./dist/${bundleName}.umd.js`,
+        format: 'umd',
+        name: 'tsmaz',
+      },
+      plugins,
     },
-    plugins: [
-      resolve({
-        preferBuiltins: false,
-      }),
-      commonjs(),
-    ],
-  },
-  {
-    external: ['tslib'],
-    input: './build/tsmaz.js',
-    output: [
-      { file: pkg.module, format: 'es' },
-      { file: pkg.main, format: 'cjs' },
-    ],
-  },
-];
+    // CommonJS + ES6
+    {
+      input: `./build/${bundleName}.js`,
+      output: [
+        { file: `./dist/${bundleName}.esm.js`, format: 'es' },
+        { file: `./dist/${bundleName}.cjs.js`, format: 'cjs' },
+      ],
+      plugins,
+    },
+    // ES6 minified
+    {
+      input: `./build/${bundleName}.js`,
+      output: {
+        file: `./dist/${bundleName}.esm.min.js`,
+        format: 'es',
+      },
+      plugins: [
+        ...plugins,
+        compiler({
+          compilation_level: 'ADVANCED_OPTIMIZATIONS',
+        }),
+      ],
+    },
+    // CommonJS minified
+    {
+      input: `./dist/${bundleName}.esm.min.js`,
+      output: {
+        file: `./dist/${bundleName}.cjs.min.js`,
+        format: 'cjs',
+      },
+    },
+    // UMD minified
+    {
+      input: `./dist/${bundleName}.esm.min.js`,
+      output: {
+        file: `./dist/${bundleName}.umd.min.js`,
+        format: 'umd',
+        name: 'tsmaz',
+      },
+    },
+  ];
+}
+
+export default tasks('tsmaz');
