5157541361f44c76a6ff4edaf2f4fdbb9e72f295
Rémi Berson <remi@cliqz.com>
2020-01-29 19:33:09 +0100

    perf: optimize codebooks generation

--- src/generate.ts
+++ src/generate.ts
@@ -11,6 +11,14 @@ class StringCounter {
     this.map.set(key, (this.map.get(key) || 0) + 1);
   }
 
+  public decr(key: string): void {
+    this.map.set(key, (this.map.get(key) || 1) - 1);
+  }
+
+  public delete(key: string): void {
+    this.map.delete(key);
+  }
+
   public update(keys: string[]): void {
     for (let i = 0; i < keys.length; i += 1) {
       this.incr(keys[i]);
@@ -22,66 +30,48 @@ class StringCounter {
   }
 }
 
-function run(
-  compress: (str: string) => Uint8Array,
-  strings: string[],
-): { totalCompressed: number; totalUncompressed: number } {
-  let totalCompressed = 0;
-  let totalUncompressed = 0;
-
-  for (let i = 0; i < strings.length; i += 1) {
-    const str = strings[i];
-    const compressed = compress(str);
-    totalCompressed += compressed.length;
-    totalUncompressed += str.length;
+function extractNgrams(str: string): string[] {
+  if (str.length === 0) {
+    return [];
   }
 
-  return { totalCompressed, totalUncompressed };
+  const ngrams: string[] = [];
+  const maxNgramSize = 70;
+
+  // Generate n-grams
+  const len = str.length;
+  for (let j = 0; j < str.length - 1; j += 1) {
+    const remainingChars = len - j;
+    for (let k = 2; k <= maxNgramSize && k <= remainingChars; k += 1) {
+      ngrams.push(str.slice(j, j + k));
+    }
+  }
+
+  return ngrams;
 }
 
-function getNextBestSubstring(strings: string[]): string {
-  const maxNgramSize = 200;
-  const substrings = new StringCounter();
+function addCounts(strings: string[], counter: StringCounter): void {
+  for (let i = 0; i < strings.length; i += 1) {
+    counter.update(extractNgrams(strings[i]));
+  }
+}
 
-  const alphaTokens = new RegExp(`[a-z0-9]{${maxNgramSize + 1},}`, 'gi');
-  const alphaDashTokens = new RegExp(`[a-z0-9_-]{${maxNgramSize + 1},}`, 'gi');
-  const alphaDashDotTokens = new RegExp(
-    `[a-z0-9_.-]{${maxNgramSize + 1},}`,
-    'gi',
-  );
+function delCounts(strings: string[], counter: StringCounter): void {
   for (let i = 0; i < strings.length; i += 1) {
     const str = strings[i];
-    if (str.length === 0) {
-      continue;
-    }
-
-    substrings.update(str.match(alphaTokens) || []);
-    substrings.update(
-      (str.match(alphaDashTokens) || []).filter(
-        t => t.indexOf('-') !== -1 || t.indexOf('_') !== -1,
-      ),
-    );
-    substrings.update(
-      (str.match(alphaDashDotTokens) || []).filter(t => t.indexOf('.')),
-    );
-
-    // Generate n-grams
-    const len = str.length;
-    for (let j = 0; j < len - 1; j += 1) {
-      const remainingChars = len - j;
-      for (let k = 2; k <= maxNgramSize && k <= remainingChars; k += 1) {
-        substrings.incr(str.slice(j, j + k));
-      }
+    const ngrams = extractNgrams(str);
+    for (let j = 0; j < ngrams.length; j += 1) {
+      counter.decr(ngrams[j]);
     }
   }
+}
 
+function getNextBestSubstring(substrings: StringCounter): string {
   // Get best substring based on length and number of occurrences
   let bestScore = 0;
   let bestSubstring = '';
-  const counts: Array<[string, number]> = Array.from(substrings.entries());
-  for (let i = 0; i < counts.length; i += 1) {
-    const substring = counts[i][0];
-    const count = counts[i][1];
+  for (const [substring, count] of substrings.entries()) {
+    // const score = count * substring.length * Math.log(substring.length);
     const score = count * substring.length ** 2.2;
     if (score > bestScore) {
       bestSubstring = substring;
@@ -94,25 +84,34 @@ function getNextBestSubstring(strings: string[]): string {
 
 function getCompressionRatio(codebook: string[], strings: string[]): number {
   const smaz = new Smaz(codebook);
-  const { totalCompressed, totalUncompressed } = run(
-    str => smaz.compress(str),
-    strings,
-  );
+  let totalCompressed = 0;
+  let totalUncompressed = 0;
+
+  for (let i = 0; i < strings.length; i += 1) {
+    const str = strings[i];
+    totalCompressed += smaz.getCompressedSize(str);
+    totalUncompressed += str.length;
+  }
+
   return 100.0 * (totalCompressed / totalUncompressed);
 }
 
 export default function generate(originalStrings: string[]): string[] {
   let strings = originalStrings;
   const codebook: string[] = [];
+  const counter = new StringCounter();
+  addCounts(strings, counter);
 
   for (let i = 0; i < 254; i += 1) {
-    const substring = getNextBestSubstring(strings);
+    const substring = getNextBestSubstring(counter);
     if (!substring) {
       console.log('No more strings', i);
       break;
     }
 
     codebook.push(substring);
+    counter.delete(substring);
+
     console.log(
       `+ ${substring} = ${getCompressionRatio(codebook, originalStrings)}%`,
     );
@@ -122,6 +121,11 @@ export default function generate(originalStrings: string[]): string[] {
       const str = strings[j];
       if (str.indexOf(substring) !== -1) {
         const parts = str.split(substring);
+
+        // Update count of all substrings
+        delCounts([str], counter);
+        addCounts(parts, counter);
+
         for (let k = 0; k < parts.length; k += 1) {
           newStrings.push(parts[k]);
         }
